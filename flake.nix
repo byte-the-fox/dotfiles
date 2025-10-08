e13892ab9189bd99aa5b2807e00aadb6ab953c8b
byte-the-fox <johnstonellie42@gmail.com>
2025-10-08 10:08:31 -0400

    Upadate flake.nix

--- flake.nix
+++ flake.nix
@@ -1,1 +1,20 @@
+{
+  description = "Configurations for all of my devices.";
 
+  inputs = {
+    # Core dependencies
+    nixpkgs.url           = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url  = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Extra dependencies
+    # TODO: Implement our own repository for packages
+
+    # Security dependencies
+    sops-nix = { url = "github:Mic92/sops-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
+    agenix   = { url = "github:ryantm/agenix";  inputs.nixpkgs.follows = "nixpkgs"; };
+
+    # Utility dependencies
+
+    # System specific configs
+  };
+}
